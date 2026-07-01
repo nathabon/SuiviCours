a8765f397a0e4c8c9c2d6a8ccd62afdbf414986e
Nathanaël Bontoux <nath.bontoux@gmail.com>
2026-07-01 19:00:07 +0200

    .

--- profcalendar/settings/prod.py
+++ profcalendar/settings/prod.py
@@ -6,7 +6,7 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default=os.environ["DATABASE_URL"],
         conn_max_age=600
     )
 }
